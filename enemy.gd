3889a6b692fef969c08937107b4411257e06f43c
MasterSlothGames <65043183+MasterSlothGames@users.noreply.github.com>
2025-05-31 18:59:38 +0200

    Enemy update

--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
+@export var static_guarding:bool
+@export var spinning_speed:float
+
 @export var field_of_vision:Area2D
 @export var obstacle_check_ray:RayCast2D
 @export var failTimer: Timer
+@export var character: Sprite2D
+
+
 
 var player_in_field:bool = false
 var player_is_seen:bool = false
@@ -13,7 +19,16 @@ func _ready() -> void:
 	field_of_vision.area_exited.connect( _on_area_2d_player_exited )
 	failTimer.timeout.connect(_on_fail_timer_timeout)
 	
-func _physics_process(_delta: float) -> void:
+func _physics_process( delta: float) -> void:
+	#--- spinning -----
+	if static_guarding:
+		field_of_vision.rotate( spinning_speed * delta ) 
+	#--- update visuals -----
+	character.global_rotation = 0.0
+	character.global_position = global_position + Vector2(0,-17)
+	field_of_vision.global_position = global_position + Vector2(0,-33)
+	
+	#--- detection ------
 	if player_in_field:
 		modulate = Color.ORANGE
 		obstacle_check_ray.look_at( player_area.global_position )
