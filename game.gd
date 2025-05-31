deddbf9171b13f51d43a12f80298b50f90acc684
Felix Lange <fjl@twurst.com>
2025-05-31 17:09:12 +0200

    fix detection for all items collected

--- game.gd
+++ game.gd
@@ -6,8 +6,11 @@ extends Control
 var items = 0
 
 func _ready() -> void:
+	rocket.max_value = goalItems
 	Signals.item_picked_up.connect(_on_item_pickedUp)
 
 func _on_item_pickedUp():
 	items = items + 1
-	rocket.value = items / goalItems
+	rocket.value = items
+	if items >= goalItems:
+		print("you are done")
