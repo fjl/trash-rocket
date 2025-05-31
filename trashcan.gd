b51afc6ac618206c2a4d4515b974c5255047dde9
C. <cmassimo@posteo.net>
2025-05-31 16:33:22 +0200

    adds trashcan collision detection for player (not working :( )

--- trashcan.gd
+++ trashcan.gd
@@ -1,3 +1,13 @@
-extends Sprite2D
+extends Node2D
 
 var containsItem = true
+
+@export var area: Area2D
+
+func _ready() -> void:
+	area.area_entered.connect(_on_area_2d_area_entered)
+
+func _on_area_2d_area_entered(_area: Area2D) -> void:
+	print("entered")
+	print_debug("trigger")
+	Signals.item_picked_up.emit()
